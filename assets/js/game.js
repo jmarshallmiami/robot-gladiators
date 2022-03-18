9ff52d53ac49f5cd682673c2c510b71455199158
Jules <j.marshallmiami@yahoo.com>
2022-03-18 18:55:08 -0500

    shop prompt accepts int values

--- assets/js/game.js
+++ assets/js/game.js
@@ -181,20 +181,18 @@ var endGame = function () {
 
 var shop = function () {
     var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
+    shopOptionPrompt = parseInt(shopOptionPrompt);
+
+    
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
-
-            // do nothing, so function will end
             break;
         default:
             window.alert("You did not pick a valid option. Try again.");
